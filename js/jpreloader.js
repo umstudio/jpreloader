a9a78cef76ed9608fe69c39594dbf32d642c9e11
Elijah Hatem <elias.hatem@gmail.com>
2016-02-23 14:37:30 -0300

    Back to v1.0 found at http://www.inwebson.com/jquery/jpreloader-a-preloading-screen-to-preload-images/

--- js/jpreloader.js
+++ js/jpreloader.js
@@ -4,9 +4,9 @@
 *
 * Name:			jPreLoader.js
 * Author:		Kenny Ooi - http://www.inwebson.com
-* Date:			July 11, 2012		
-* Version:		2.1.1 (ums fork)
-* Example:		http://www.inwebson.com/demo/jpreloader-v2/
+* Date:			January 01, 2012		
+* Version:		1.0
+* Example:	
 *	
 */
 
@@ -22,40 +22,100 @@
 		splashID: '#jpreContent',
 		showSplash: true,
 		showPercentage: true,
-		autoClose: true,
-		closeBtnText: 'Start!',
-		onetimeLoad: false,
 		debugMode: false,
 		splashFunction: function() {}
 	}
 	
-	//cookie
-	var getCookie = function() {
-		if( jpreOptions.onetimeLoad ) {
-			var cookies = document.cookie.split('; ');
-			for (var i = 0, parts; (parts = cookies[i] && cookies[i].split('=')); i++) {
-				if ((parts.shift()) === "jpreLoader") {
-					return (parts.join('='));
+	var getImages = function(element) {
+		$(element).find('*:not(script)').each(function() {
+			var url = "";
+
+			if ($(this).css('background-image').indexOf('none') == -1) {
+				url = $(this).css('background-image');
+				if(url.indexOf('url') != -1) {
+					var temp = url.match(/url\((.*?)\)/);
+					url = temp[1].replace(/\"/g, '');
 				}
+			} else if ($(this).get(0).nodeName.toLowerCase() == 'img' && typeof($(this).attr('src')) != 'undefined') {
+				url = $(this).attr('src');
 			}
-			return false;
-		} else {
-			return false;
-		}
-		
+			
+			if (url.length > 0) {
+				items.push(url);
+			}
+		});
 	}
-	var setCookie = function(expires) {
-		if( jpreOptions.onetimeLoad ) {
-			var exdate = new Date();
-			exdate.setDate( exdate.getDate() + expires );
-			var c_value = ((expires==null) ? "" : "expires=" + exdate.toUTCString());
-			document.cookie="jpreLoader=loaded; " + c_value;
+	
+	var preloading = function() {
+		for (var i = 0; i < items.length; i++) {
+			loadImg(items[i]);
 		}
 	}
 	
-	//create jpreLoader UI
-	var createContainer = function() {
+	var loadImg = function(url) {
+		var imgLoad = new Image();
+		$(imgLoad)
+		.load(function() {
+			completeLoading();
+		})
+		.error(function() {
+			errors.push($(this).attr('src'));
+			completeLoading();
+		})
+		.attr('src', url);
+	}
+	
+	var completeLoading = function() {
+		current++;
+
+		var per = Math.round((current / items.length) * 100);
+		$(jBar).stop().animate({
+			width: per + '%'
+		}, 500, 'linear');
 		
+		if(jpreOptions.showPercentage) {
+			$(jPer).text(per+"%");
+		}
+		
+		if(current >= items.length) {
+		
+			current = items.length;
+			
+			if (jpreOptions.debugMode) {
+				var error = debug();
+				
+			} 
+			loadComplete();
+		}
+	}
+	
+	var loadComplete = function() {
+		$(jBar).stop().animate({
+			width: '100%'
+		}, 500, 'linear', function() {
+			$(jOverlay).fadeOut(800, function() {
+				$(jOverlay).remove();
+				onComplete();
+			});
+		});	
+	}
+	
+	var debug = function() {
+		if(errors.length > 0) {
+			var str = 'ERROR - IMAGE FILES MISSING!!!\n\r'
+			str	+= errors.length + ' image files cound not be found. \n\r';	
+			str += 'Please check your image paths and filenames:\n\r';
+			for (var i = 0; i < errors.length; i++) {
+				str += '- ' + errors[i] + '\n\r';
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	var createContainer = function(tar) {
+
 		jOverlay = $('<div></div>')
 		.attr('id', 'jpreOverlay')
 		.css({
@@ -113,125 +173,6 @@
 			.appendTo(jLoader)
 			.html('Loading...');
 		}
-		if( !jpreOptions.autoclose ) {
-			jButton = $('<div></div>')
-			.attr('id', 'jpreButton')
-			.on('click', function() {
-				loadComplete();
-			})
-			.css({
-				position: 'relative',
-				height: '100%'
-			})
-			.appendTo(jLoader)
-			.text(jpreOptions.closeBtnText)
-			.hide();
-		}
-	}
-	
-	//get all images from css and <img> tag
-	var getImages = function(element) {
-		$(element).find('*:not(script)').each(function() {
-			var url = "";
-
-			if ($(this).css('background-image').indexOf('none') == -1 && $(this).css('background-image').indexOf('-gradient') == -1) {
-				url = $(this).css('background-image');
-				if(url.indexOf('url') != -1) {
-					var temp = url.match(/url\((.*?)\)/);
-					url = temp[1].replace(/\"/g, '');
-				}
-			} else if ($(this).get(0).nodeName.toLowerCase() == 'img' && typeof($(this).attr('src')) != 'undefined') {
-				url = $(this).attr('src');
-			}
-			
-			if (url.length > 0) {
-				items.push(url);
-			}
-		});
-	}
-	
-	//create preloaded image
-	var preloading = function() {
-		for (var i = 0; i < items.length; i++) {
-			if(loadImg(items[i]));
-		}
-	}
-	var loadImg = function(url) {
-		var imgLoad = new Image();
-		$(imgLoad)
-		.load(function() {
-			completeLoading();
-		})
-		.error(function() {
-			errors.push($(this).attr('src'));
-			completeLoading();
-		})
-		.attr('src', url);
-	}
-	
-	//update progress bar once image loaded
-	var completeLoading = function() {
-		current++;
-
-		var per = Math.round((current / items.length) * 100);
-		$(jBar).stop().animate({
-			width: per + '%'
-		}, 500, 'linear');
-		
-		if(jpreOptions.showPercentage) {
-			$(jPer).text(per+"%");
-		}
-		
-		//if all images loaded
-		if(current >= items.length) {
-			current = items.length;
-			setCookie();	//create cookie
-			
-			if(jpreOptions.showPercentage) {
-				$(jPer).text("100%");
-			}
-			
-			//fire debug mode
-			if (jpreOptions.debugMode) {
-				var error = debug();
-			}
-			
-			
-			//max progress bar
-			$(jBar).stop().animate({
-				width: '100%'
-			}, 500, 'linear', function() {
-				//autoclose on
-				if( jpreOptions.autoClose )
-					loadComplete();
-				else
-					$(jButton).fadeIn(1000);
-			});	
-		}	
-	}
-		
-	//triggered when all images are loaded
-	var loadComplete = function() {
-		$(jOverlay).fadeOut(800);
-		setTimeout(function() {
-			$(jOverlay).remove();
-			onComplete();	//callback function
-		}, 800);
-	}
-
-	//debug mode
-	var debug = function() {
-		if(errors.length > 0) {
-			var str = 'ERROR - IMAGE FILES MISSING!!!\n\r'
-			str	+= errors.length + ' image files cound not be found. \n\r';	
-			str += 'Please check your image paths and filenames:\n\r';
-			for (var i = 0; i < errors.length; i++) {
-				str += '- ' + errors[i] + '\n\r';
-			}
-			return true;
-		} else {
-			return false;
-		}
 	}
 	
 	$.fn.jpreLoader = function(options, callback) {
@@ -242,22 +183,10 @@
 			onComplete = callback;
 		}
 		
-		//show preloader once JS loaded
-		$('body').css({
-			'display': 'block'
-		});
-		
-		return this.each(function() {
-			if( !(getCookie()) ) {
-				createContainer();
-				getImages(this);
-				preloading();
-			}
-			else {	//onetime load / cookie is set
-				$(jpreOptions.splashID).remove();
-				onComplete();
-			}
-		});
+		createContainer(this);
+		getImages(this);
+		preloading();
+        return this;
     };
 
 })(jQuery);
